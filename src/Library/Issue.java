0aed103e66fb85afa00b7e70539251c784579369
Akanksha Pawar <pawarakanksha1101@gmail.com>
2025-09-19 18:19:05 +0530

    Update Issue.java

--- src/Library/Issue.java
+++ src/Library/Issue.java
@@ -1,1 +1,35 @@
+package Library;
 
+import java.sql.Date;
+
+public class Issue {
+    private int issueId;
+    private int bookId;
+    private int memberId;
+    private Date issueDate;
+    private Date returnDate;
+
+    public Issue(int issueId, int bookId, int memberId, Date issueDate, Date returnDate) {
+        this.issueId = issueId;
+        this.bookId = bookId;
+        this.memberId = memberId;
+        this.issueDate = issueDate;
+        this.returnDate = returnDate;
+    }
+
+    public int getIssueId() {
+        return issueId;
+    }
+    public int getBookId() {
+        return bookId;
+    }
+    public int getMemberId() {
+        return memberId;
+    }
+    public Date getIssueDate() {
+        return issueDate;
+    }
+    public Date getReturnDate() {
+        return returnDate;
+    }
+}
