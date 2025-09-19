096046cabe18bf20db0799adef9d97eb6ef3f6c6
Akanksha Pawar <pawarakanksha1101@gmail.com>
2025-09-19 18:17:05 +0530

    Update Member.java

--- src/Library/Member.java
+++ src/Library/Member.java
@@ -1,1 +1,23 @@
+package Library;
 
+public class Member {
+    private int memberId;
+    private String name;
+    private String email;
+
+    public Member(int memberId, String name, String email) {
+        this.memberId = memberId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
